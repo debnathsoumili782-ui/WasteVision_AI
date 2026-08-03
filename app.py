dcf201d54a314efbfa6216798586825ca602558a
Soumili Debnath <debnathsoumili782@gmail.com>
2026-08-04 03:21:51 +0530

    Configure Railway port

--- app.py
+++ app.py
@@ -367,4 +367,5 @@ def delete_history(id):
 
     return redirect(url_for("detection_history"))
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
